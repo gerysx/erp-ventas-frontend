dcb7bd59b494a5cf179a93359d716e8ff4050f1f
Gerysx <geralvarez95rgmail.com>
2025-05-29 10:31:30 +0200

    Fallo layout

--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -1,5 +1,4 @@
-
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../auth/auth.service';
 import { CommonModule } from '@angular/common';
@@ -9,13 +8,15 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule, RouterOutlet, RouterModule],
   templateUrl: './main-layout.component.html',
-  styleUrls: ['./main-layout.component.scss']
+  styleUrls: ['./main-layout.component.scss'],
 })
-export class MainLayoutComponent {
+export class MainLayoutComponent implements OnInit {
   usuarioCorreo = '';
   esAdmin = false;
 
-  constructor(private auth: AuthService, private router: Router) {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
     const usuario = this.auth.obtenerUsuario();
     this.usuarioCorreo = usuario?.correo ?? '';
     this.esAdmin = usuario?.rol === 'ADMIN';
